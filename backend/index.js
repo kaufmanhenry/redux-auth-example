2b688875d32677ee3980c078ba8a17e5e0183922
Henry Kaufman <hkaufman19@gmail.com>
2017-07-02 10:57:03 -0600

    Added some backend code for awful authentication

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ module.exports = (app, callback) => {
   const router = app.get('router')();
 
   router.post('/login', (req, res) => {
-    res.send({
-      token: 'this_is_a_token'
+    const { username, password } = req.body;
+
+    // Super awful authentication. Should not be used in production
+    if (username === 'test' && password === 'test') {
+      return res.send({
+        status: 200,
+        message: 'Successfully authenticated'
+      });
+    }
+
+    return res.send({
+      status: 403,
+      message: 'Failed to authenticated.'
     });
   });
 
